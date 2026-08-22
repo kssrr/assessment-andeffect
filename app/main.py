cdf8ffc279cb468f4ba2abf5382f3010d0550f68
kssrr <lennart.kasserra@gmail.com>
2026-08-22 10:50:10 +0200

    geht auch schlanker...

--- app/main.py
+++ app/main.py
@@ -1,33 +1,11 @@
-from contextlib import asynccontextmanager
 from datetime import date
-from fastapi import FastAPI, Depends, Query, Request
-from sqlalchemy import func
+from fastapi import FastAPI, Depends, Query
 from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app import models, schemas
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    db = SessionLocal()
-    try:
-        # wenn der user keine date range angibt
-        # wollen wir einfach alle daten (ganze range)
-        # ausgeben. Min & max date vorab im Kontext 
-        # speichern, um nicht jedes mal wenn der Endpoint
-        # aufgerufen wird min & max queries machen zu
-        # müssen:
-        dmin, dmax = db.query(
-            func.min(models.Availability.date),
-            func.max(models.Availability.date),
-        ).one()
-        app.state.date_bounds = (dmin, dmax)
-    finally:
-        db.close()
-    yield 
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 def get_db():
     db = SessionLocal()
@@ -42,22 +20,20 @@ def root():
 
 @app.get("/list", response_model=list[schemas.AvailabilityResponse])
 def list_items(
-    request: Request,
     dmin: date | None = Query(None, description="From (YYYY-MM-DD)"),
     dmax: date | None = Query(None, description="From (YYYY-MM-DD)"),
+    service: str | None = Query(None, description="Service"),
     db: Session = Depends(get_db)
 ):
-    # falls keine date range angegeben, ganze date
-    # range benutzen:
-    default_min, default_max = request.app.state.date_bounds
-    dmin = dmin or default_min
-    dmax = dmax or default_max
+    query = db.query(models.Availability)
 
-    result = (
-        db
-        .query(models.Availability)
-        .filter(models.Availability.date.between(dmin, dmax))
-        .all()
-    )
+    if dmin is not None:
+        query = query.filter(models.Availability.date >= dmin)
+    if dmax is not None:
+        query = query.filter(models.Availability.date <= dmax)
+    if service is not None:
+        query = query.filter(models.Availability.service == service)
+
+    result = query.all()
 
     return result
